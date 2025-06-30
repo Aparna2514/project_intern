b17c64a8f1817062e98d51f8a13fa7a31225d603
Aparna2514 <aparnaverma0412@gmail.com>
2025-06-30 22:54:43 +0530

    Basic design

---
+++ digital-agency/public/script.js
@@ -0,0 +1,13 @@
+// Button click
+document.querySelector("button").addEventListener("click", () => {
+  alert("Let's grow your brand together! 🚀");
+});
+
+// Lottie animation
+lottie.loadAnimation({
+  container: document.getElementById("lottie-animation"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "https://assets4.lottiefiles.com/packages/lf20_tll0j4bb.json"
+});
